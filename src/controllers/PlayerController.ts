bdda1302c1d8d7375b37f9217ade47a1d27d264b
Dmitriy Dobrynin <dm.dobrinin@gmail.com>
2023-03-02 14:48:20 +0300

    Add some tuning before physics addition

--- src/controllers/PlayerController.ts
+++ src/controllers/PlayerController.ts
@@ -11,24 +11,72 @@ import {
 } from "@babylonjs/core";
 
 class PlayerController extends AbstractMesh {
-  private _mesh: Mesh;
-  private _camera: UniversalCamera;
+  /**
+   * Двигается ли игрок вперед?
+   */
   private _movingForward = false;
+
+  /**
+   * Двигается ли игрок назад?
+   */
   private _movingBack = false;
+
+  /**
+   * Двигается ли игрок влево?
+   */
   private _movingLeft = false;
+
+  /**
+   * Двигается ли игрок вправо?
+   */
   private _movingRight = false;
-  private _isRunning = false;
-  private _isZooming = false;
+
+  /**
+   * Меш игрока (моделька)
+   */
+  private _playerMesh: AbstractMesh;
+
+  /**
+   * Рост игрока
+   */
   private _characterHeight = 1.8;
 
+  /**
+   * Бежит ли игрок?
+   */
+  private _isRunning = false;
+
+  /**
+   * Оружие игрока
+   */
   private _weapon: AbstractMesh;
 
-  private _playerWrapper: AbstractMesh;
+  /**
+   * Прицелился ли игрок?
+   */
+  private _isZooming = false;
 
+  /**
+   * Скорость ходьбы
+   */
   private _walkSpeed = 10;
+
+  /**
+   * Скорость бега
+   */
   private _runSpeed = 25;
+
+  /**
+   * Массив материалов, которыми игрок может "стрелять" (используется для декалей)
+   */
   private _splatters: StandardMaterial[];
-  _scene: Scene;
+
+  private _camera: UniversalCamera;
+
+  /**
+   * "Обертка" игрока. Меш, который будет передвигаться по сцене с помощью кнопок управления
+   */
+  private _playerWrapper: AbstractMesh;
 
   constructor(
     camera: UniversalCamera,
@@ -42,21 +90,21 @@ class PlayerController extends AbstractMesh {
 
     this._splatters = splatters;
 
-    this._mesh = playerMesh;
-    this._mesh.position = new Vector3(
-      this._mesh.position.x,
+    this._playerMesh = playerMesh;
+    this._playerMesh.position = new Vector3(
+      this._playerMesh.position.x,
       this._characterHeight / 2,
-      this._mesh.position.z
+      this._playerMesh.position.z
     );
 
     this._camera = camera;
-    this._camera.parent = this._mesh;
+    this._camera.parent = this._playerMesh;
     this._camera.position.y = this._characterHeight;
 
     this._playerWrapper = this;
 
-    this._mesh.setParent(this._playerWrapper);
-    this._mesh.isVisible = false;
+    this._playerMesh.setParent(this._playerWrapper);
+    this._playerMesh.isVisible = false;
 
     this._listenEvents();
     this._calculateMovement();
@@ -75,6 +123,9 @@ class PlayerController extends AbstractMesh {
     let once = false;
 
     this._scene.registerBeforeRender(() => {
+      /**
+       * Количество времени (в секундах), которое прошло между текущим и предыдущим кадром (фреймом)
+       */
       const deltaTime = this._scene.getEngine().getDeltaTime() / 1000;
 
       const cameraDirection = this._camera
@@ -125,7 +176,7 @@ class PlayerController extends AbstractMesh {
 
       // left click (can't find enum)
       if (event.button === 0) {
-        const origin = this._mesh
+        const origin = this._playerMesh
           .getAbsolutePosition()
           .subtract(new Vector3(0, -this._characterHeight, 0));
 
@@ -134,20 +185,16 @@ class PlayerController extends AbstractMesh {
         const raycastHit = this._scene.pickWithRay(ray);
 
         if (raycastHit.hit) {
-          setTimeout(() => {
-            const decal = CreateDecal("decal", raycastHit.pickedMesh, {
-              position: raycastHit.pickedPoint,
-              normal: raycastHit.getNormal(true),
-              size: new Vector3(1, 1, 1),
-            });
+          const decal = CreateDecal("decal", raycastHit.pickedMesh, {
+            position: raycastHit.pickedPoint,
+            normal: raycastHit.getNormal(true),
+            size: new Vector3(1, 1, 1),
+          });
 
-            decal.material =
-              this._splatters[
-                Math.floor(Math.random() * this._splatters.length)
-              ];
+          decal.material =
+            this._splatters[Math.floor(Math.random() * this._splatters.length)];
 
-            decal.setParent(raycastHit.pickedMesh);
-          }, 50);
+          decal.setParent(raycastHit.pickedMesh);
         }
       } else if (event.button === 2) {
         // right click (can't find enum)
